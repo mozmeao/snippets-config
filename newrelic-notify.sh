30f10071ad62d162d0a4e8aad9bc0b1b582e3987
Jonathan Petto <jpetto@users.noreply.github.com>
2019-07-08 11:04:25 -0500

    Review fixes #2:
    - Use regex to get image hash from yaml (instead of asking k8s)
    - Alphabetize some things

--- newrelic-notify.sh
+++ newrelic-notify.sh
@@ -3,15 +3,11 @@
 set -ex pipefail
 
 NEWRELIC_USER=gitlab
-# TODO: where/how to get newrelic api key?
-# retrieve from somewhere on disk or a service?
+
 API_KEY=$(< ~/.newrelic-api-key)
 
-# get the image value from the base deployment matching the K8S_NAMESPACE
-FULL_IMAGE="kubectl get deploy ${K8S_NAMESPACE} -n ${K8S_NAMESPACE} -o jsonpath=\"{$.spec.template.spec.containers[0].image}\""
-
 # last 7 characters of the image value will be the hash
-GIT_COMMIT_SHORT="${FULL_IMAGE: -7}"
+GIT_COMMIT_SHORT="grep -oP '(image: .+?:)\K.*' ${DEPLOYMENT}/web-deploy.yaml"
 
 # go tell new relic
 curl -s -H "x-api-key:${API_KEY}" \
