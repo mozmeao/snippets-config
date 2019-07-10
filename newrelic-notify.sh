dd35bba4886ee33c3603704b872b570c08626acc
Jonathan Petto <jpetto@users.noreply.github.com>
2019-07-09 19:51:27 -0500

    Add new relic notification script post deploy. (#27)
    * Add new relic notification script post deploy.
    
    * Review fixes:
    
    - Consolidate kubectl commands
    - Get actual API key
    - Update jsonpath to image to be more specific
    
    * Review fixes #2:
    
    - Use regex to get image hash from yaml (instead of asking k8s)
    - Alphabetize some things
    
    * Review fixes part 3:
    
    - Wrap subshell command to capture output
    - Improve comments
    - Notify new relic prior to waiting for/running acceptance tests

---
+++ newrelic-notify.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -ex pipefail
+
+NEWRELIC_USER=gitlab
+
+API_KEY=$(< ~/.newrelic-api-key)
+
+# the image value in the yaml contains the first 7 digits of the git commit hash
+# yaml line format is as follows:
+# image: mozorg/snippets:283065b
+GIT_COMMIT_SHORT="$(grep -oP '(image: .+?:)\K.*' ${DEPLOYMENT}/web-deploy.yaml)"
+
+# go tell new relic
+curl -s -H "x-api-key:${API_KEY}" \
+     -d "deployment[app_name]=${NEWRELIC_APP}" \
+     -d "deployment[revision]=${GIT_COMMIT_SHORT}" \
+     -d "deployment[user]=${NEWRELIC_USER}" \
+     https://api.newrelic.com/deployments.xml
