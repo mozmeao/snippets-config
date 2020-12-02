1d427d3f21f5561b8e9e42fdef37fb994d930bd3
Giorgos Logiotatidis <seadog@sealabs.net>
2020-12-02 15:05:21 +0200

    Update config for new clusters.

--- newrelic-notify.sh
+++ newrelic-notify.sh
@@ -9,7 +9,7 @@ API_KEY=$(< ~/.newrelic-api-key)
 # the image value in the yaml contains the first 7 digits of the git commit hash
 # yaml line format is as follows:
 # image: mozorg/snippets:283065b
-GIT_COMMIT_SHORT="$(grep -oP '(image: .+?:)\K.*' ${DEPLOYMENT}/web-deploy.yaml)"
+GIT_COMMIT_SHORT="$(grep -oP '(image: .+?:)\K.*' ${DEPLOYMENT}/web.yaml)"
 
 # go tell new relic
 curl -s -H "x-api-key:${API_KEY}" \
